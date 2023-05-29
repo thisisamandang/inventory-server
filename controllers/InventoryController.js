0e6c3a78a29fc87b9cbc8b22cd43ae65559bef6f
Aman <thisisamandang@gmail.com>
2023-05-30 04:40:44 +0530

    .

--- controllers/InventoryController.js
+++ controllers/InventoryController.js
@@ -132,17 +132,16 @@ module.exports.getLowStock = async (req, res, next) => {
     const { userid } = req.params;
     const inventoryItems = await Inventory.find({
       user: userid,
-    })
-      .$where("this.stocks < this.low")
-      .select([
-        "itemName",
-        "category",
-        "itemCode",
-        "description",
-        "unit",
-        "stocks",
-        "low",
-      ]);
+      $expr: { $lt: ["$stocks", "$low"] }, // Compare stocks with low using
+    }).select([
+      "itemName",
+      "category",
+      "itemCode",
+      "description",
+      "unit",
+      "stocks",
+      "low",
+    ]);
     return res.json(inventoryItems);
   } catch (ex) {
     next(ex);
